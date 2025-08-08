323985670c907256f5a1c73f6b54b404fe293baf
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2025-08-07 22:45:17 -0300

    Simulacao Propagacao Agrcs - inclusão comentarios e correções

--- simulacao_AC.py
+++ simulacao_AC.py
@@ -24,6 +24,7 @@ class AutomatoCelular:
         self.tamanho = tamanho
 
     def inicializador_grade(self):
+        # Inicializa o solo como 90% fértil e 10% com um agricultor inicial
         num_agrics_inics = int(self.tamanho * self.tamanho * 0.1)
         ind_agrics = random.sample(range(self.tamanho * self.tamanho), num_agrics_inics)
 
@@ -32,35 +33,36 @@ class AutomatoCelular:
             self.grade[linha, coluna] = 1
 
     def contar_vizinhos(self, linha, coluna, estado):
-        contador = 0 
+        contador = 0
+        # Itera sobre os vizinhos, incluindo diagonais (Vizinhança de Moore)
         for i in range(max(0, linha - 1), min(self.tamanho, linha + 2)):
             for j in range(max(0, coluna - 1), min(self.tamanho, coluna + 2)):
-                if (i != linha or j != coluna) and self.grade[i, j] == estado:
+                if (i, j) != (linha, coluna) and self.grade[i, j] == estado:
                     contador += 1
         return contador
 
-    def atualizar_grade(self):
+    def atualizar(self):
         nova_grade = self.grade.copy()
 
         for i in range(self.tamanho):
             for j in range(self.tamanho):
                 estado_atual = self.grade[i, j]
 
-                if estado_atual == 0:
+                if estado_atual == 0:  # Solo Fértil
                     vizinhos_agrics = self.contar_vizinhos(i, j, 1)
                     if vizinhos_agrics >= 2:
                         nova_grade[i, j] = 1
-                
-                elif estado_atual == 1:
+            
+                elif estado_atual == 1:  # Agricultor
                     self.idade_agrics[i, j] += 1
                     if self.idade_agrics[i, j] >= TEMPO_DEGRADACAO:
                         nova_grade[i, j] = 2
                         self.idade_agrics[i, j] = 0
 
-                elif estado_atual == 2:
+                elif estado_atual == 2:  # Solo Degradado
                     if random.random() < PROBABILIDADE_REGENERACAO_SOLO:
                         nova_grade[i, j] = 0
-        
+
         self.grade = nova_grade
 
     def executar_simulacao(self):
@@ -70,20 +72,23 @@ class AutomatoCelular:
         ax.set_yticks([])
         img = ax.imshow(self.grade, cmap=mapa_cores, norm=norma, animated=True)
 
-        forma = [plt.Rectangle((0, 0), 1, 1, fc=c) for c in cores]
+        # Cria a legenda
+        patches = [plt.Rectangle((0, 0), 1, 1, fc=c) for c in cores]
         rotulos = ['Solo Fértil', 'Agricultor', 'Solo\nDegradado']
-
-        ax.legend(forma, rotulos, loc='upper right', bbox_to_anchor=(1.2, 0.95), borderaxespad=0)
+        
+        ax.legend(patches, rotulos, loc='upper left', bbox_to_anchor=(1.02, 0.95), borderaxespad=0)
 
         leg_geracao = ax.text(1.02, 0.95, '', transform=ax.transAxes, fontsize=10, verticalalignment='bottom')
         
-        def atualizar(geracao):
-            self.atualizar_grade()
+        def atualizar_plot(geracao):
+            self.atualizar()
             img.set_array(self.grade)
+            # Atualiza o texto da geracao atual
             leg_geracao.set_text(f'Geração: {geracao + 1}/{GERACOES}')
             return img, leg_geracao
         
-        animacao = FuncAnimation(fig, atualizar, frames=GERACOES, interval=200, blit=False, repeat=False)
+        # Altere a variavel interval para controlar a velocidade da animação
+        animacao = FuncAnimation(fig, atualizar_plot, frames=GERACOES, interval=200, blit=False, repeat=False)
         plt.tight_layout()
         plt.show()
 
@@ -92,7 +97,3 @@ if __name__ == "__main__":
     automato = AutomatoCelular(TAMANHO_GRADE)
     automato.inicializador_grade()
     automato.executar_simulacao()
-        
-
-                    
-
