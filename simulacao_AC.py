da8f35a78b77ca6bbbbbb05a85040e37135dc272
Aurilene Bagagi <aurilenebagagi.es@gmail.com>
2025-08-07 22:08:45 -0300

    Simulacao Propagacao Agrcs - primeira versao

--- simulacao_AC.py
+++ simulacao_AC.py
@@ -16,3 +16,83 @@ mapa_cores = mcolors.ListedColormap(cores)
 limites = [0, 1, 2, 3]
 norma = mcolors.BoundaryNorm(limites, mapa_cores.N)
 
+
+class AutomatoCelular:
+    def __init__(self, tamanho):
+        self.grade = np.zeros((tamanho, tamanho), dtype=int)
+        self.idade_agrics = np.zeros((tamanho, tamanho), dtype=int)
+        self.tamanho = tamanho
+
+    def inicializador_grade(self):
+        num_agrics_inics = int(self.tamanho * self.tamanho * 0.1)
+        ind_agrics = random.sample(range(self.tamanho * self.tamanho), num_agrics_inics)
+
+        for ind in ind_agrics:
+            linha, coluna = divmod(ind, self.tamanho)
+            self.grade[linha, coluna] = 1
+
+    def contar_vizinhos(self, linha, coluna, estado):
+        contador = 0 
+        for i in range(max(0, linha - 1), min(self.tamanho, linha + 2)):
+            for j in range(max(0, coluna - 1), min(self.tamanho, coluna + 2)):
+                if (i != linha or j != coluna) and self.grade[i, j] == estado:
+                    contador += 1
+        return contador
+
+    def atualizar_grade(self):
+        nova_grade = self.grade.copy()
+
+        for i in range(self.tamanho):
+            for j in range(self.tamanho):
+                estado_atual = self.grade[i, j]
+
+                if estado_atual == 0:
+                    vizinhos_agrics = self.contar_vizinhos(i, j, 1)
+                    if vizinhos_agrics >= 2:
+                        nova_grade[i, j] = 1
+                
+                elif estado_atual == 1:
+                    self.idade_agrics[i, j] += 1
+                    if self.idade_agrics[i, j] >= TEMPO_DEGRADACAO:
+                        nova_grade[i, j] = 2
+                        self.idade_agrics[i, j] = 0
+
+                elif estado_atual == 2:
+                    if random.random() < PROBABILIDADE_REGENERACAO_SOLO:
+                        nova_grade[i, j] = 0
+        
+        self.grade = nova_grade
+
+    def executar_simulacao(self):
+        fig, ax = plt.subplots()
+        ax.set_title('Simulação de Propagação Agricultores e Degradação do Solo')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        img = ax.imshow(self.grade, cmap=mapa_cores, norm=norma, animated=True)
+
+        forma = [plt.Rectangle((0, 0), 1, 1, fc=c) for c in cores]
+        rotulos = ['Solo Fértil', 'Agricultor', 'Solo\nDegradado']
+
+        ax.legend(forma, rotulos, loc='upper right', bbox_to_anchor=(1.2, 0.95), borderaxespad=0)
+
+        leg_geracao = ax.text(1.02, 0.95, '', transform=ax.transAxes, fontsize=10, verticalalignment='bottom')
+        
+        def atualizar(geracao):
+            self.atualizar_grade()
+            img.set_array(self.grade)
+            leg_geracao.set_text(f'Geração: {geracao + 1}/{GERACOES}')
+            return img, leg_geracao
+        
+        animacao = FuncAnimation(fig, atualizar, frames=GERACOES, interval=200, blit=False, repeat=False)
+        plt.tight_layout()
+        plt.show()
+
+
+if __name__ == "__main__":
+    automato = AutomatoCelular(TAMANHO_GRADE)
+    automato.inicializador_grade()
+    automato.executar_simulacao()
+        
+
+                    
+
